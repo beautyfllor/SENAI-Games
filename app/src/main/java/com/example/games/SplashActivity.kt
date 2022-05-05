0dedaf4b1d569574aa3431481fca33934bb22cea
FLORBELA FREITAS OLIVEIRA <21276327@CFP127ALUNO.LOCAL>
2022-05-05 16:58:00 -0300

    splash concluído, home/main em andamento

---
+++ app/src/main/java/com/example/games/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.games
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Timer().schedule(2000) {abrirMain()}
+    }
+
+    private fun abrirMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
