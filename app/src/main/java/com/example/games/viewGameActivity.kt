c43d3e8b4e43881c3ae80699feeb24547f74b4e7
FLORBELA FREITAS OLIVEIRA <21276327@CFP127ALUNO.LOCAL>
2022-05-12 16:55:29 -0300

    Game view pronta

---
+++ app/src/main/java/com/example/games/viewGameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.games
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class viewGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_game)
+    }
+}
